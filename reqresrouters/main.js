c1d4cb096ebe9495e8f3f35268e514e7e4d20860
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-05-13 19:11:14 +0530

    Update main.js

--- reqresrouters/main.js
+++ reqresrouters/main.js
@@ -6,14 +6,25 @@ const port = 5000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello worrrrrrrrrld");
+  res.send("Hello worrrrrrrrrld pranali");
   console.log("hey its a get request");
 });
 
 app.post("/", (req, res) => {
-  res.send("Hello worrld");
+  res.send("Hello worrld ");
   console.log("hey its a post request");
+});
+
+app.put("/", (req, res) => {
+  res.send("Hello worrld puuu");
+  console.log("hey its a put request");
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+//Chaining of request
+app.get("/index",(req,res) => {
+  res.sendFile('templates/index.html',{root:__dirname});
+  //res.status() res.download() res.end() res.json() res.redirect()
+  console.log("hey its a index request");
 });
